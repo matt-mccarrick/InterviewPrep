deda27c336c10786313967818cb983da7fbe0b3a
Matt McCarrick <matthew.r.mccarrick@gmail.com>
2017-02-13 19:30:25 -0800

    added kth to last for linked lists

---
+++ InterviewPrep/LinkedLists/KthToLast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.LinkedLists
+{
+    public class KthToLast
+    {
+        public LNode ReturnKthToLast(LNode head, int k)
+        {
+            int counter = 0;
+            var runner = head;
+            while (runner != null)
+            {
+                if (counter == k)
+                {
+                    head = head.Next;
+                }
+                else
+                {
+                    counter ++;
+                }
+                runner = runner.Next;
+            }
+            return head;
+        }
+    }
+}
